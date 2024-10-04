62b2e686e3e950748014228a45ed58bb3ec7f808
Nakonditsiha <Nakonditsiha@209-15>
2024-10-04 14:26:11 +0200

    Добавьте файлы проекта.

---
+++ хлоя маф/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel.Design;
+
+try
+{ 
+    Console.Write("Введите x:");
+    double x = double.Parse(Console.ReadLine());
+    Console.Write("Введите y:");
+    double y = double.Parse(Console.ReadLine());
+    Console.Write("Значение радиуса:");
+    double R = double.Parse(Console.ReadLine());
+    if ((x * x + y * y) < R * R) Console.WriteLine("Находиться внутри окружности") ;
+    else Console.WriteLine("Вне окружности");
+}
+catch
+{
+    Console.WriteLine("Error");
+}
+
